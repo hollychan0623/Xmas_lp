c3ca56dd87dd07f41bee8081f36495640c50ad70
hollychan0623 <56242205+hollychan0623@users.noreply.github.com>
2020-11-20 16:59:00 +0900

    バッグまで実装
    ・スムーススクロール
    ・バッグ途中まで

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,18 @@ $(function(){
     });
 });
 
+// スムーススクロール
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+
 
 $(function() {
     var topBtn = $('#page-top');    
@@ -56,6 +68,9 @@ $(function() {
       });
     });
   })(jQuery);
+
+
+ 
 
 //   メリークリスマスアニメーション
 /*-----
